62fbf8eff441ba54663aec2742e5184aa84e4b32
Gabriela Silva <vigabiesdivision@gmail.com>
2023-08-10 12:18:10 -0300

    flex

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.top}>
+        <ImageBackground
+          style={styles.img}
+          source={require("./assets/favicon.png")}
+        />
+      </View>
+
+      <View style={styles.body}>
+        <Text>bbbbbbbbbbbbbb</Text>
+      </View>
+
+      <View style={styles.bottom}>
+        <Text>ccccccccc</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +31,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  //---------------------------------------------------
+  top: {
+    flex: 0.5,
+    backgroundColor: "#fff",
+    flexDirection: "row-reverse",
+    marginTop: "5%",
+    marginLeft: "5%",
+  },
+
+  botao: {
+    backgroundColor: "#fff",
+  },
+
+  img: {
+    height: 50,
+    width: 50,
+  },
+
+  //---------------------------------------------------
+
+  body: {
+    flex: 5,
+    backgroundColor: "#eb4034",
+  },
+
+  //---------------------------------------------------
+
+  bottom: {
+    flex: 0.5,
+    backgroundColor: "#d634eb",
   },
 });
