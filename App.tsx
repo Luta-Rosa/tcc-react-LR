0a113a5c0e1cb7b7305f80681cbd452d5a8e63b6
Gabriela Silva <vigabiesdivision@gmail.com>
2023-08-24 12:20:01 -0300

    vector icons - baixado

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from "expo-status-bar";
+import React from "react";
 import {
   StyleSheet,
   Text,
@@ -6,63 +7,12 @@ import {
   TouchableOpacity,
   ImageBackground,
 } from "react-native";
-import Title from './src/components/home'
+import Title from './src/pages/home';
+
+import Home from "./src/pages/home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.top}>
-        <ImageBackground
-          style={styles.img}
-          source={require("./assets/favicon.png")}
-        />
-      </View>
-
-      <View style={styles.body}>
-        <Title/>
-      </View>
-
-      <View style={styles.bottom}>
-        <Text>ccccccccc</Text>
-      </View>
-    </View>
-  );
+    <Home />
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  //---------------------------------------------------
-  top: {
-    flex: 0.5,
-    backgroundColor: "#fff",
-    flexDirection: "row-reverse",
-    marginTop: "5%",
-    marginLeft: "5%",
-  },
-
-  botao: {
-    backgroundColor: "#fff",
-  },
-
-  img: {
-    height: 50,
-    width: 50,
-  },
-
-  //---------------------------------------------------
-
-  body: {
-    flex: 5,
-    backgroundColor: "#eb4034",
-  },
-
-  //---------------------------------------------------
-
-  bottom: {
-    flex: 0.5,
-    backgroundColor: "#d634eb",
-  },
-});
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,30 @@
+import {
+  View,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  Modal,
+  TextInput,
+  TouchableWithoutFeedback,
+} from "react-native";
+import Styles from "./styles";
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+
+export default function Home() {
+  return (
+    <View style={Styles.container}>
+      <View style={Styles.top}>
+        <Text>aaaa</Text>
+      </View>
+
+      <View style={Styles.body}>
+        <Text>bbbbb</Text>
+      </View>
+
+      <View style={Styles.bottom}>
+        <Text>ccccc</Text>
+      </View>
+    </View>
+  );
+}
