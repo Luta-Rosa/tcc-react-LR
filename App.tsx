8e922116284596562fd68f08c7cd0b803ad5a5a7
Gabriela Silva <vigabiesdivision@gmail.com>
2023-09-21 11:13:04 -0300

    clear

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
-import React from "react";
-import { View, SafeAreaView, StatusBar } from "react-native";
-import Title from "./src/pages/home";
-
-import Home from "./src/pages/home";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <View style={{flex: 1}}>
-      <StatusBar/>
-      <Home />
-      </View>
-    </SafeAreaView>
-  )
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
